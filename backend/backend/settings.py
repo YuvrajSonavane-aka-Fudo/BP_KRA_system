b3fdeed966623da4a60ad0ed58646aed2fab9293
Yuvi <yuvasmail1@gmail.com>
2026-04-08 15:00:45 +0530

    backend basic setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kra_cycle',
+    'corsheaders',
 ]
 
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +71,8 @@ TEMPLATES = [
         },
     },
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
