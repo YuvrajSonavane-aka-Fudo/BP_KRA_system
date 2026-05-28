4879306fedbdf314882d868974ba43be278ebac7
Yuvi <yuvasmail1@gmail.com>
2026-05-28 11:51:11 +0530

    reverted some changes

--- backend/kra_assignment/urls.py
+++ backend/kra_assignment/urls.py
@@ -8,9 +8,9 @@ from .views import (
 
 urlpatterns = [
     path('employees/',EmployeeListView.as_view()),
-    path('kra/cycles/<int:cycle_id>/assignments/bulk/',KRABulkAssignmentEnrolView.as_view()),
-    path('kra/assignments/<int:employee_kra_cycle_id>/',KRAAssignmentUpdateDeleteView.as_view()),
-    path('kra/assignments/clone-from/',KRAAssignmentCloneView.as_view()),
+    path('kra/cycles/<int:cycle_id>/assignments/bulk',KRABulkAssignmentEnrolView.as_view()),
+    path('kra/assignments/<int:employee_kra_cycle_id>',KRAAssignmentUpdateDeleteView.as_view()),
+    path('kra/assignments/clone-from',KRAAssignmentCloneView.as_view()),
     
     
     
--- frontend/src/config/env.js
+++ frontend/src/config/env.js
@@ -1,5 +1,5 @@
 const env = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL  || 'https://bp-kra-system.onrender.com/api/v1/' || 'http://localhost:8000/api/v1/',
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL  || 'http://localhost:8000/api/v1/',
   SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL || '',
   SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY || '',
   SSO_CLIENT_ID: import.meta.env.VITE_SSO_CLIENT_ID || '',
